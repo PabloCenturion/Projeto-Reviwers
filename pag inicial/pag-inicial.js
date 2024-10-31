583d78585648ad2623868ab73373895846915464
miguelgarbo <miguelggarbo@gmail.com>
2024-10-30 21:51:28 -0300

    ++tela inicial

--- pag inicial/pag-inicial.js
+++ pag inicial/pag-inicial.js
@@ -1,11 +1,40 @@
-// Seleciona todos os quadrados
-const quadrados = document.querySelectorAll('.quadrado');
 
-quadrados.forEach(quadrado => {
-    quadrado.addEventListener('click', () => {
-        // Remove a classe 'sobreposto' de todos os quadrados
-        quadrados.forEach(q => q.classList.remove('sobreposto'));
-        // Adiciona a classe 'sobreposto' apenas ao quadrado clicado
-        quadrado.classList.add('sobreposto');
-    });
-});
+// function changeImgMovie(){
+
+// const listaFilmes = JSON.parse(localStorage.getItem("listaFilmes"));
+
+// console.log(listaFilmes.length)
+
+// for(let i = 0;i<listaFilmes.length;i++){
+    
+//     const imgMovie = document.getElementById('img-movie-display');
+
+//     imgMovie.src = listaFilmes[i].movieCover
+
+// }
+
+
+// }
+
+// changeImgMovie()
+
+
+let i = 0;
+
+function changeImgMovie() {
+
+    const listaFilmes = JSON.parse(localStorage.getItem("filmesLancamentos"));
+
+    console.log(listaFilmes)
+    
+        const imgMovie = document.getElementById('img-movie-display');
+        
+        // Atualiza a imagem com a capa do filme atual
+        imgMovie.src = listaFilmes[i].cover;
+        
+        // Atualiza o índice para o próximo filme
+        i = (i + 1) % listaFilmes.length; // Volta ao início se chegar ao fim
+   
+}
+
+changeImgMovie();
